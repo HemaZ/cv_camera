d1575989f49b6b5fdb97273e83c98013255773bc
Ibrahim Essam <ibrahim.essam1995@gmail.com>
2020-07-01 17:32:03 +0200

    updated node

--- src/cv_camera_node.cpp
+++ src/cv_camera_node.cpp
@@ -2,23 +2,18 @@
 
 #include "cv_camera/driver.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "cv_camera");
   ros::NodeHandle private_node("~");
   cv_camera::Driver driver(private_node, private_node);
-
-  try
-  {
+  ROS_INFO("Updated Node !!");
+  try {
     driver.setup();
-    while (ros::ok())
-    {
+    while (ros::ok()) {
       driver.proceed();
       ros::spinOnce();
     }
-  }
-  catch (cv_camera::DeviceError &e)
-  {
+  } catch (cv_camera::DeviceError &e) {
     ROS_ERROR_STREAM("cv camera open failed: " << e.what());
     return 1;
   }
